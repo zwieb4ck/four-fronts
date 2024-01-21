93101bec8e4134f2b1d6ee0f230b579387799b9f
Paul-Christian Wolter <pwolter@hotmail.de>
2024-01-21 12:44:18 +0100

    swiched engine

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,6 @@
-import "./style.css";
-import { Scene, Game, WEBGL, GameObjects } from "phaser";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-const canvas = document.getElementById("game") as HTMLCanvasElement;
-
-class GameScene extends Scene {
-  private text!: GameObjects.Text;
-  private button!: GameObjects.Rectangle;
-  constructor() {
-    super("scene-game");
-  }
-
-  create() {
-    this.button = this.add.rectangle(1218 / 2, 526 / 2, 250, 50, 0xff0000, 1);
-    this.text = this.add.text(1218 / 2, 526 / 2, "Play FourFronts", {
-      color: "#FFF",
-      fontFamily: "monospace",
-      fontSize: "26px",
-    });
-    this.text.setOrigin(0.5, 0.5);
-  }
-
-  update(time: number, delta: number) {}
-}
-
-const config = {
-  type: WEBGL,
-  canvas,
-  width: 1218,
-  height: 526,
-  scene: [GameScene],
-  parent: "game-window",
-};
-
-new Game(config);
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
